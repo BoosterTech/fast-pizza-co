3eb91f546e067e4acf4e93c40aa44c7a91b797c5
Darek Podczasik <boostertech@mail.com>
2024-08-30 21:42:55 +0100

    <SearchOrder> component added, isLoading condition moved to <main> section

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,18 +1,19 @@
 import { Link, Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Loader from "./Loader";
-
+import SearchOrder from "../features/order/SearchOrder";
 
 const AppLayout = () => {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
   return (
     <div className="layout">
-      {isLoading && <Loader />}
       <header>
         <Link to="/">Fast React Pizza</Link>
+        <SearchOrder />
       </header>
       <main>
+        {isLoading && <Loader />}
         <Outlet />
       </main>
       <CartOverview />
