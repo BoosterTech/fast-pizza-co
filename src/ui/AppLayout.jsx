b1b85979d3cbb51ea4d941e9be09996ddf406495
Darek Podczasik <boostertech@mail.com>
2024-09-02 20:55:10 +0100

    styles applied

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,20 @@
-import { Link, Outlet, useNavigation } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview";
-import Loader from "./Loader";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link, Outlet, useNavigation } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview';
+import Loader from './Loader';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 const AppLayout = () => {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
   return (
     <div className="layout">
-      <header>
-        <Link to="/">Fast React Pizza</Link>
+      <header className="bg-yellow-500 uppercase">
+        <Link to="/" className="tracking-widest">
+          Fast React Pizza
+        </Link>
         <SearchOrder />
+        <Username />
       </header>
       <main>
         {isLoading && <Loader />}
