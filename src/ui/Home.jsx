cada0d9d7985678c049e9d98874a90b5fef4a44b
Darek Podczasik <boostertech@mail.com>
2024-09-04 21:19:16 +0100

    styles applied

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,12 +1,12 @@
-import CreateUser from '../features/user/CreateUser';
+import CreateUser from "../features/user/CreateUser";
 
 function Home() {
   return (
-    <div className="mt-10 text-center">
+    <div className="mt-10 px-4 text-center sm:my-16">
       <h1 className="mb-8 text-center text-xl font-semibold">
         The best pizza.
         <br />
-        <span className="text-yellow-500">
+        <span className="text-yellow-500 md:text-3xl">
           Straight out of the oven, straight to you.
         </span>
       </h1>
