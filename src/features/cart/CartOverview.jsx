2ce233558fdfa005ceb2e853cca39e58b180546f
Darek Podczasik <boostertech@mail.com>
2024-09-22 12:59:08 +0100

    total cart quantinty pizzas implemented

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,10 +1,14 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartPrice } from "./cartSlice";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartPrice);
+
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200">
       <p className="space-x-4 font-semibold text-stone-300">
-        <span>23 pizzas</span>
+        <span>{totalCartQuantity} pizzas</span>
         <span>$23.45</span>
       </p>
       <Link to="cart">Open cart &rarr;</Link>
