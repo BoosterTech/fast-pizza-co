4600d9a7496e8e81f665bd4c09bf4fe45e6ee026
Darek Podczasik <boostertech@mail.com>
2024-09-22 14:53:33 +0100

    "Clear cart" button functionality added

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,13 +1,14 @@
 import CartItem from "../cart/CartItem";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
-import { getCart } from "./cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, getCart } from "./cartSlice";
 import { getUser } from "../user/userSlice";
 
 function Cart() {
   const cart = useSelector(getCart);
   const username = useSelector(getUser);
+  const dispatch = useDispatch();
 
   return (
     <div className="px-4 py-3">
@@ -25,7 +26,9 @@ function Cart() {
       <div className="mt-6 space-x-2">
         <Button to="/order/new">Order pizzas</Button>
 
-        <Button type="secondary">Clear Cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear Cart
+        </Button>
       </div>
     </div>
   );
