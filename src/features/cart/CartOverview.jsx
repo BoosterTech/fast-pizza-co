41d8132e5364dc41d1eb8880e3cc38fc850474fe
Darek Podczasik <boostertech@mail.com>
2024-09-22 13:11:23 +0100

    totalCartPrice renamed to totalCartQuantity, new totalCartPrice  variable added and implemented

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,15 +1,16 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getTotalCartPrice } from "./cartSlice";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
 
 function CartOverview() {
-  const totalCartQuantity = useSelector(getTotalCartPrice);
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
 
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200">
       <p className="space-x-4 font-semibold text-stone-300">
         <span>{totalCartQuantity} pizzas</span>
-        <span>$23.45</span>
+        <span>${totalCartPrice}</span>
       </p>
       <Link to="cart">Open cart &rarr;</Link>
     </div>
