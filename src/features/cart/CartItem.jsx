0429f19e7577c364c2dcb5764e900e4b0857f3af
Darek Podczasik <boostertech@mail.com>
2024-09-22 12:57:28 +0100

    pizzaId added to <li> element of CartItem

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -5,7 +5,7 @@ function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+    <li key={pizzaId} className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
