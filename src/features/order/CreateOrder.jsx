58a15438a7251eaa3f08ba55d6ebecf19b24adae
Darek Podczasik <boostertech@mail.com>
2024-09-25 21:05:06 +0100

    geolocation datas added to from submition

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -117,6 +117,15 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.longitude && position.latitude
+                ? `${position.latitude}, ${position.longitude}`
+                : ""
+            }
+          />
           <Button disabled={isSubmitting || isLoadingAddress}>
             {isSubmitting
               ? "Placing order..."
@@ -137,6 +146,8 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "true",
   };
+
+  console.log(order);
 
   const errors = {};
   if (!isValidPhone(order.phone))
