a644ce7ac8d4df2f92bef080f61768079e45600b
Dariusz Podczasik <boostertech@mail.com>
2025-01-25 22:23:06 +0000

    configure for gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
   base: "/Fast-Pizza-Co/",
+  build: {
+    outDir: "docs",
+  },
 });
